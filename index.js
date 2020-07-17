8baea1813a67ff99a897d6cb17a6800b496aef62
Psychol0g1st <46202519+Psychol0g1st@users.noreply.github.com>
2020-07-17 15:56:17 +0200

    Test a new mode

--- index.js
+++ index.js
@@ -11,14 +11,33 @@ function newElement(){
 	$("h1").text("New element:");
 	$("h4").text("Level: " + level);
 	var pad = Math.floor(Math.random()*4);
-	setTimeout(function(){
+	sequence.push(pad);
+	i=0;
+	showPads();
+	/*for(var i=0; i<sequence.length; i++){
+		$("."+pads[sequence[i]]).fadeOut(fadeSpeed).fadeIn(fadeSpeed);
+		sounds[sequence[i]].play();
+	}*/
+	/*setTimeout(function(){
 		$("."+pads[pad]).fadeOut(fadeSpeed).fadeIn(fadeSpeed);
 		sounds[pad].play();
 		sequence.push(pad);
 		$("h1").text("Repeat All");
-	}, 1000);
+	}, 1000);*/
 	clickNum = 0;
 	
+}
+function showPads(){
+	$("."+pads[sequence[i]]).fadeOut(fadeSpeed).fadeIn(fadeSpeed);
+	sounds[sequence[i]].play();
+	i++;
+	if(i < sequence.length){
+		setTimeout(showPads, fadeSpeed+300);
+	} else{
+		setTimeout(function(){
+			$("h1").text("Repeat All");
+		}, 500);
+	}
 }
 function checkRepeat(){
 	if(isStart){
@@ -28,10 +47,12 @@ function checkRepeat(){
 			clickNum++;
 			if(clickNum === level){
 				level++;
-				if(level%2 === 0){
-					fadeSpeed -= 10;
+				if(level%3 === 0){
+					if(fadeSpeed-decrement > 50){
+						fadeSpeed -= decrement;
+					}
 				}
-				newElement();
+				setTimeout(newElement, 1000);
 			}
 		} else {
 			sounds[sounds.length-1].play();
@@ -52,6 +73,8 @@ var pads = ["green", "red", "yellow", "blue"];
 var clickNum = 0;
 var sounds = [];
 var fadeSpeed = 200;
+var i =0;
+var decrement = 20;
 sounds.push(new Audio("sounds/green.mp3"));
 sounds.push(new Audio("sounds/red.mp3"));
 sounds.push(new Audio("sounds/yellow.mp3"));
